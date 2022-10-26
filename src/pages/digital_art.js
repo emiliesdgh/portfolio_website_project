33579ac6419b135c861592fe69ac90708e35a6a7
em <emilie.grandjean@epfl.ch>
2022-10-26 17:36:45 +0200

    gatsby tuto 4 fini -> go faire tuto 5

--- src/pages/digital_art.js
+++ src/pages/digital_art.js
@@ -1,14 +1,32 @@
 import * as React from 'react'
+import { graphql } from 'gatsby'
 import Layout from '../components/layout'
 import Seo from '../components/seo'
 
-const DigitalArtPage = () => {
+const DigitalArtPage = ({data}) => {
   return (
-    <Layout pageTitle="Digital Art section">
-      <p>This section is a groupement of all my digital arts.</p>
+    <Layout pageTitle="Digital Art section / blog tuto 4">
+      <ul>
+      {
+        data.allFile.nodes.map(node => (
+          <li key={node.name}>
+            {node.name}
+          </li>
+        ))
+      }
+      </ul>
     </Layout>
   )
 }
+export const query = graphql`
+  query {
+    allFile {
+      nodes {
+        name
+      }
+    }
+  }
+`
 
 export const Head = () => <Seo title = "My Digital Art"/>
 
